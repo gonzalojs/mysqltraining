97c1c9ae28e7fa7c4cba7ba66c36b75d03b0d1a5
gonzalojs <eogutier@gmail.com>
2019-12-06 22:01:58 -0300

    posts created

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ const app = express()
 
 // create table
 app.get('/createpoststable', (req, res) => {
-  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255) PRIMARY KEY id)'
+  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))'
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    console.log(result)
+    res.send('Post Table Created...')
+  })
 })
 
 
